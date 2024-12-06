d602c864a9fb117a1968b04fe7f13ba6a998a6dc
Lars Doucet <lars.doucet@gmail.com>
2024-12-06 17:05:49 -0600

    IDE metadata

---
+++ openavmkit/modeling.py
@@ -0,0 +1,180 @@
+from typing import Union
+
+import numpy as np
+import statsmodels.api as sm
+import pandas as pd
+import xgboost
+from sklearn.metrics import mean_squared_error
+from statsmodels.regression.linear_model import RegressionResults
+from xgboost import XGBRegressor
+
+PredictionModel = Union[RegressionResults, XGBRegressor]
+
+class PredictionResults:
+	ind_var: str
+	dep_vars: list[str]
+	y: pd.Series
+	y_pred: np.ndarray
+	mse: float
+	rmse: float
+	r2: float
+	adj_r2: float
+
+	def __init__(self,
+			ind_var: str,
+			dep_vars: list[str],
+			y: pd.Series,
+			y_pred: np.ndarray
+	):
+		self.ind_var = ind_var
+		self.dep_vars = dep_vars
+		self.y = y
+		self.y_pred = y_pred
+
+		self.mse = mean_squared_error(y, y_pred)
+		self.rmse = np.sqrt(self.mse)
+		self.r2 = 1 - self.mse / np.var(y)
+
+		# Adjusted R2 = 1 – [(1-R2)*(n-1)/(n-k-1)]
+		#
+		# where:
+		#
+		# R2: The R2 of the model
+		# n: The number of observations
+		# k: The number of predictor variables
+
+		n = len(y_pred)
+		k = len(dep_vars)
+
+		self.adj_r2 = 1 - ((1 - self.r2)*(n-1)/(n-k-1))
+
+
+class DataSplit:
+	X: pd.DataFrame
+	y: pd.Series
+	X_train: pd.DataFrame
+	y_train: pd.Series
+	X_test: pd.DataFrame
+	y_test: pd.Series
+
+	def __init__(self,
+			df: pd.DataFrame,
+			ind_var: str,
+			dep_vars: list[str],
+			test_train_frac: float = 0.8,
+			random_seed: int = 1337
+	):
+		# separate df in train & test:
+		np.random.seed(random_seed)
+
+		df_train = df.sample(frac=test_train_frac).reset_index(drop=True)
+		df_test = df.drop(df_train.index).reset_index(drop=True)
+
+		self.X = df[dep_vars]
+		self.y = df[ind_var]
+
+		self.X_train = df_train[dep_vars]
+		self.y_train = df_train[ind_var]
+
+		self.X_test = df_test[dep_vars]
+		self.y_test = df_test[ind_var]
+
+
+class ModelResults:
+	type: str
+	ind_var: str
+	dep_vars: list[str]
+	model: PredictionModel
+	pred_test: PredictionResults
+	pred_full: PredictionResults
+
+	def __init__(self,
+			type: str,
+			ind_var: str,
+			dep_vars: list[str],
+			model: PredictionModel,
+			y_test: pd.Series,
+			y_pred_test: np.ndarray,
+			y_full: pd.Series,
+			y_pred_full: np.ndarray
+	):
+		self.type = type
+		self.ind_var = ind_var
+		self.dep_vars = dep_vars
+		self.model = model
+		self.pred_test = PredictionResults(ind_var, dep_vars, y_test, y_pred_test)
+		self.pred_full = PredictionResults(ind_var, dep_vars, y_full, y_pred_full)
+
+	def summary(self):
+		print(f"Model type: {self.type}")
+		# Print the # of rows in test & full set
+		# Print the MSE, RMSE, R2, and Adj R2 for test & full set
+		print(f"-->Test set, rows: {len(self.pred_test.y)}")
+		print(f"---->MSE    : {self.pred_test.mse}")
+		print(f"---->RMSE   : {self.pred_test.rmse}")
+		print(f"---->R2     : {self.pred_test.r2}")
+		print(f"---->Adj R2 : {self.pred_test.adj_r2}")
+		print(f"")
+		print(f"-->Full set, rows: {len(self.pred_full.y)}")
+		print(f"---->MSE    : {self.pred_full.mse}")
+		print(f"---->RMSE   : {self.pred_full.rmse}")
+		print(f"---->R2     : {self.pred_full.r2}")
+		print(f"---->Adj R2 : {self.pred_full.adj_r2}")
+		print(f"")
+
+	def __str__(self):
+		if self.type == "mra":
+			return str(self.model.summary())
+		elif self.type == "xgboost":
+			return "XGBoost ModelResults"
+
+
+def run_mra(
+		df: pd.DataFrame,
+		ind_var: str,
+		dep_vars: list[str],
+		intercept: bool = True
+):
+		ds = DataSplit(df, ind_var, dep_vars)
+
+		if intercept:
+			ds.X_train = sm.add_constant(ds.X_train)
+			ds.X_test = sm.add_constant(ds.X_test)
+			ds.X = sm.add_constant(ds.X)
+
+		linear_model = sm.OLS(ds.y_train, ds.X_train)
+		fitted_model = linear_model.fit()
+
+		# predict on test set:
+		y_pred_test = fitted_model.predict(ds.X_test)
+
+		# predict on the full set:
+		y_pred_full = fitted_model.predict(ds.X)
+
+		# gather the predictions
+		results = ModelResults("mra", ind_var, dep_vars, fitted_model, ds.y_test, y_pred_test, ds.y, y_pred_full)
+
+		return results
+
+
+def run_xgboost(
+		df: pd.DataFrame,
+		ind_var: str,
+		dep_vars: list[str]
+):
+	ds = DataSplit(df, ind_var, dep_vars)
+
+	xgboost_model = xgboost.XGBRegressor(
+		n_estimators=100,
+		max_depth=4,
+		learning_rate=0.1,
+		random_state=42
+	)
+	xgboost_model.fit(ds.X_train, ds.y_train)
+
+	y_pred_test = xgboost_model.predict(ds.X_test)
+	y_pred_full = xgboost_model.predict(ds.X)
+
+	results = ModelResults("xgboost", ind_var, dep_vars, xgboost_model, ds.y_test, y_pred_test, ds.y, y_pred_full)
+
+	return results
---
+++ openavmkit/synthetic_data.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+
+
+def generate_basic():
+	data = {
+		"key": [],
+		"bldg_area_finished_sqft": [],
+		"land_area_sqft": [],
+		"bldg_quality_num": [],
+		"bldg_condition_num": [],
+		"bldg_age_years": [],
+		"land_value": [],
+		"bldg_value": [],
+		"total_value": [],
+		"distance_from_cbd": [],
+		"latitude": [],
+		"longitude": []
+	}
+
+	latitude_center = 29.760762
+	longitude_center = -95.361937
+
+	height = 0.5
+	width =  0.4
+
+	nw_lat = latitude_center - width/2
+	nw_lon = longitude_center - height/2
+
+	size = 100
+
+	base_land_value = 250
+	base_bldg_value = 25
+	quality_value = 5
+
+	# set a random seed:
+	np.random.seed(1337)
+
+	for y in range(0, size):
+		for x in range(0, size):
+
+			_x = x/size
+			_y = y/size
+
+			latitude = nw_lat + (width * _x)
+			longitude = nw_lon + (height * _y)
+
+			dist_x = abs(_x - 0.5)
+			dist_y = abs(_y - 0.5)
+			dist_center = (dist_x**2 + dist_y**2)**0.5
+
+			# base value with exponential falloff from center:
+			_base_land_value = base_land_value - 1
+			land_value_per_land_sqft = 1 + (_base_land_value * (1 - dist_center))
+
+			key = f"{x}-{y}"
+			land_area_sqft = np.random.randint(21780, 43560)
+			bldg_area_finished_sqft = np.random.randint(1000, 2500)
+			bldg_quality_num = np.clip(np.random.normal(3, 1), 0, 6)
+			bldg_condition_num = np.clip(np.random.normal(3, 1), 0, 6)
+			bldg_age_years = np.clip(np.random.normal(20, 10), 0, 100)
+			land_value = land_area_sqft * land_value_per_land_sqft
+
+			bldg_value_per_sqft = base_bldg_value + (quality_value * bldg_quality_num)
+
+			depreciation_from_age = min(0.0, 1 - (bldg_age_years / 100))
+			depreciation_from_condition = min(0.0, 1 - (bldg_condition_num / 6))
+
+			total_depreciation = (depreciation_from_age + depreciation_from_condition)/2
+
+			bldg_value_per_sqft = bldg_value_per_sqft * (1 - total_depreciation)
+			bldg_value = bldg_area_finished_sqft * bldg_value_per_sqft
+
+			total_value = land_value + bldg_value
+
+			data["key"].append(str(key))
+			data["bldg_area_finished_sqft"].append(bldg_area_finished_sqft)
+			data["land_area_sqft"].append(land_area_sqft)
+			data["bldg_quality_num"].append(bldg_quality_num)
+			data["bldg_condition_num"].append(bldg_condition_num)
+			data["bldg_age_years"].append(bldg_age_years)
+			data["land_value"].append(land_value)
+			data["bldg_value"].append(bldg_value)
+			data["total_value"].append(total_value)
+			data["distance_from_cbd"].append(dist_center)
+			data["latitude"].append(latitude)
+			data["longitude"].append(longitude)
+
+	df = pd.DataFrame(data)
+	return df
